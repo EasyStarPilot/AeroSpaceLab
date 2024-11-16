2a84cf3560dd12d4d113a9cf64a2fff29f3c790e
EasyStarPilot <EasyStarPilot@gmx.de>
2024-11-16 14:06:58 +0100

    Finished

--- Taschenrechner.c
+++ Taschenrechner.c
@@ -1,72 +1,85 @@
 #include <stdio.h>
 
-void main()
-{
-    printf("rechenart 1:a+b 2:a-b 3:a*b 4:a/b 5:a^b");
-    int rechenart = 0;
-    scanf("%i", rechenart);
-    printf("a:");
-    int a = 0;
-    scanf("%i", a);
-    printf("b:");
-    int b = 0;
-    scanf("%i", b);
-    double loesung = 0;
-    switch (rechenart)
-    {
-    case 1:
-        loesung = Plus(int a, int b);
-        break;
-    case 2:
-        loesung = Minus(a, b);
-        break;
-    case 3:
-        loesung = Mal(a, b);
-        break;
-    case 4:
-        loesung = Geteilt(a, b);
-        break;
-    case 5:
-        loesung = Hoch(a, b);
-        break;
+    int Plus(int a,int b);
+    int Minus(int a,int b);
+    int Mal(int a,int b);
+    int Geteilt(int a,int b);
+    int Hoch(int a,int b);
+    int Mod(int a,int b);
 
-    default:
-        printf("Fehler!\n");
-        break;
+
+    int main() {
+        int a = 0;
+        int b = 0;
+        double Loesung=0;
+        int Rechenart=0;
+        printf("Rechenart 1:a+b 2:a-b 3:a*b 4:a/b 5:a^b 6:a mod b ");
+        scanf("%d",&Rechenart);
+        printf("a:");
+        scanf("%d",&a);
+        printf("b:");
+        scanf("%d",&b);
+        switch (Rechenart)
+        {
+        case 1:
+            Loesung=Plus(a,b);
+            break;
+        
+        case 2:
+            Loesung=Minus(a,b);
+            break;
+        case 3:
+            Loesung=Mal(a,b);
+            break;
+        case 4:
+            Loesung=Geteilt(a,b);
+            break;
+        case 5:
+            Loesung=Hoch(a,b);
+            break; 
+        case 6:
+            Loesung=Mod(a,b);
+            break;  
+        default:
+             printf("Keine Rechnung");
+            break;
+        }
+        
+        printf("Loesung: %f" , Loesung);
     }
-    printf("loesung: " + (loesung);"%d");
-}
-double Plus(int a, int b)
-{
-    double loesung = 0;
-    loesung = a + b;
-    return loesung;
-}
-double Minus(int a, int b)
-{
-    double loesung = 0;
-    loesung = a - b;
-    return loesung;
-}
-double Mal(int a, int b)
-{
-    double loesung = 0;
-    loesung = a * b;
-    return (loesung);
-}
-double Geteilt(int a, int b)
-{
-    double loesung = 0;
-    loesung = (double)a / b;
-    return (loesung);
-}
-double Hoch(int a, int b)
-{
-    double loesung = 1;
-    while (b >= 1)
-    {
-        loesung = loesung * a;
-        b = b - 1;
+    int Plus(int a,int b){
+        double Loesung = 0;
+        Loesung = a + b;
+        return Loesung;
     }
-    return (loesung);
-}
+    int Minus(int a,int b){
+        double Loesung = 0;
+        Loesung = a - b;
+        return Loesung;
+    }
+    int Mal(int a,int b){
+        double Loesung = 0;
+        Loesung = a * b;
+        return (Loesung);
+    }
+    int Geteilt(int a,int b){
+        double Loesung = 0;
+        Loesung = (double) a / b;
+        return (Loesung);
+    }
+    int Hoch(int a,int b){
+        double Loesung  = 1;
+        while(b>=1){
+            Loesung = Loesung*a;
+            b= b-1;
+        }
+        return (Loesung);
+    }
+    int Mod(int a,int b){
+        double Loesung  = 1;
+        while(a>=b){
+            a= a-b;            
+        }
+        Loesung = a;
+        return (Loesung);
+    }
